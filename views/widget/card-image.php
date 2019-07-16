83c6ec0c56888c1157d0e2d78f3a8f3eb5a4dd64
Alexzzl <805119233@qq.com>
2019-07-16 18:53:35 +0800

    preview image

--- views/widget/card-image.php
+++ views/widget/card-image.php
@@ -1,7 +1,24 @@
-<p class="card-img">
-    <?php foreach (json_decode($imgs) as $img): ?>
-        <a href="javascript:void(0)" data-toggle="modal" data-target="#previewModal">
-            <img src="/images/upload/<?= $img ?>">
-        </a>
-    <?php endforeach; ?>
-</p>
+<?php
+
+use yii\bootstrap\Html;
+use yii\bootstrap\Modal;
+
+?>
+
+    <p class="card-img">
+        <?php foreach(json_decode($imgs) as $img) : ?>
+            <a href="javascript:void(0)" data-toggle="modal" data-target="#previewModal"><img src="/images/upload/<?=$img?>"></a>
+        <?php endforeach ?>
+    </p>
+
+<?php
+Modal::begin([
+    'options' => [
+        'id' => 'previewModel',
+    ],
+]) ?>
+<?= Html::a(Html::icon('glyphicon glyphicon-chevron-left'), 'javascrpt:void(0)', ['class' => 'preview-img-btn prev-img']); ?>
+<?= Html::img('/images/upload/default.jpg', ['class' => 'previewModal-img']); ?>
+<?= Html::a(Html::icon('glyphicon glyphicon-chevron-right'), 'javascrpt:void(0)', ['class' => 'preview-img-btn next-img']); ?>
+
+<?php Modal::end(); ?>
--- web/js/blog.js
+++ web/js/blog.js
@@ -28,3 +28,22 @@ function preview(filenames) {
     // 更换表单提交路由
     $(".upload-blog-form").attr('action', '?r=blog/repost');
 }
+
+var previewObj;
+function replaceImg(obj) {
+    var src = $(obj).find('img').attr('scr');
+    if (src != undefined){
+        $(".previewModel-img").attr('src',src);
+        previewObj = obj;
+    }
+}
+$('#previewModal').on('show.bs.modal', function (e) {
+    replaceImg(e.relatedTarget);
+});
+$(".next-img").click(function () {
+    // 寻找当前对象的下一个对象
+    replaceImg($(previewObj).next());
+});
+$(".prev-img").click(function () {
+    replaceImg($(previewObj).prev());
+});
