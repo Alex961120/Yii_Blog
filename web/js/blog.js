f950da521c793091e88934243dfefd36e0e48816
Alexzzl <805119233@qq.com>
2019-07-16 16:06:30 +0800

    click upload

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,8 +1,8 @@
 // 模拟文件上传操作 并将上传文件提交到iframe
 var form;
-// 点击上传按钮  -  获取表单对象 - 执行file按钮
-$(".btn-form-img").clock(function () {
-    form = $(this).parent("form");
+// 点击上传按钮 - 获取表单对象 - 执行file按钮
+$(".btn-form-img").click(function () {
+    form = $(this).parents("form");
     $(form).find("#uploadform-imagefiles").click();
 
     // 如果表单域内容发生改变，给表单添加属性
@@ -13,10 +13,10 @@ $(".btn-form-img").clock(function () {
     });
 });
 
-function preview(filename) {
+function preview(filenames) {
     var images = input = '';
     // 遍历显示文件信息
-    $($.parseJSON(filename)).each(function (i, k) {
+    $($.parseJSON(filenames)).each(function (i, k) {
         // 添加一个图片标签，为它设置服务器返回的图片 src
         images += "<img class='preview-img' src='images/upload/" + k + "'>";
         input += "<input type='hidden' name='filenames[]' value='" + k + "'>";
@@ -24,7 +24,7 @@ function preview(filename) {
 
     // 将 img 组合放入窗口中，显示多张图片
     $(form).find(".upload-images").html(images + input).css("clear", "both");
-    $("upload-form").attr('target', '');
+    $(".upload-form").attr('target', '');
     // 更换表单提交路由
-    $(".upload-blog-form").attr('action','?r=blog/store');
+    $(".upload-blog-form").attr('action', '?r=blog/store');
 }
